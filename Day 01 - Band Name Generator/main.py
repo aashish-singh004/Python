a660c7f881129f844ec70504bf538801b86d4eb0
aashish-singh004 <aashishfall2026@gmail.com>
2026-06-30 11:56:41 +0530

    bandnamegenerator.py

--- Day 01 - Band Name Generator/main.py
+++ Day 01 - Band Name Generator/main.py
@@ -1,1 +1,6 @@
-
+print("Welcome to the Og Band Name Generator")
+city = input("What city did you grow up?\n")
+#\n is used to make it look more good and professional
+pet = input("What is the name of a pet?\n")
+print(f"Your band name could be {city} {pet}")
+#f string is used here , instead of doing it + way thing..
