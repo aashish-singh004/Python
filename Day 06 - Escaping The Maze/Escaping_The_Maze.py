581cee15cc63d4ca7133b815ff30e7cebe0f7982
aashish-singh004 <aashishfall2026@gmail.com>
2026-07-14 21:14:39 +0530

    Update Escaping_The_Maze.py

--- Day 06 - Escaping The Maze/Escaping_The_Maze.py
+++ Day 06 - Escaping The Maze/Escaping_The_Maze.py
@@ -1,1 +1,14 @@
-
+#visit reeborg's World and select python and then maze problem.
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+#it was kinda confusing at first and took me so much time but its still worth it and gonna help me build strong foundation haha.
